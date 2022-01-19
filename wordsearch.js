7cf6091df6d2414f38d3ff49495f0a334eedd750
alparslanustaa <alparslanustaa@gmail.com>
2022-01-19 19:57:34 +0000

    pair programin exercise

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,14 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) return true;
     }
-}
+    const verticalJoin = letters.map(ls => ls.join(''))
+    for (i of verticalJoin) {
+        if(i.includes(word)) return true;
+    }
+    return false;
+};
 
 module.exports = wordSearch
+
